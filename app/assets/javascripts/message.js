4d560b45944c5d54743dc86ebf3e56481e1621e7
MemiMizukami <mm@cornerprinting.com>
2020-11-17 22:29:07 +0900

    aa

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="main_chat__message_list">
+      `<div class="main_chat__message_list" data-message-id=${message.id}>
          <div class="message_information">
            <div class="user_name">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="main_chat__message_list">
+      `<div class="main_chat__message_list" data-message-id=${message.id}>
          <div class="message_information">
            <div class="user_name">
              ${message.user_name}
@@ -37,31 +37,57 @@ $(function(){
          </div>
        </div>`
      return html;
-   };
- }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('form')[0].reset();
-    // フォームをカラにする
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    // スクロール
+    };
+   }
+
+   $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.main_chat__message_list').append(html)
+       $('.main_chat__message_list').animate({ scrollTop: $('.main_chat__message_list')[0].scrollHeight});
+       $('form')[0].reset();
+       $('.main_chat__message_form').prop('disabled', false);
+     })
+     .fail(function() {
+       alert("メッセージ送信に失敗しました");
+     }); 
+   })
+
+ var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: "get",
+    dataType: 'json',
+    data: {id: last_message_id}
   })
-  .fail(function(){
-    alert('error');
-  });
-  return false;
-})
+    .done(function(message){
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert('error');
+    });
+  }
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
